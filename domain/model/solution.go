55a051a4fa9231d1a5b94f7fbe9459ea60c32b7a
Leonard Pak <leopak2000@gmail.com>
2023-11-19 19:10:29 +0300

    add approve field to solution

--- domain/model/solution.go
+++ domain/model/solution.go
@@ -3,12 +3,14 @@ package model
 import "time"
 
 type SolutionFromClass struct {
-	ID         int       `json:"id"`
-	HomeworkID int       `json:"hwID"`
-	StudentID  int       `json:"studentID"`
-	Text       string    `json:"text,omitempty"`
-	CreateTime time.Time `json:"createTime"`
-	File       string    `json:"file"`
+	ID                int       `json:"id"`
+	HomeworkID        int       `json:"hwID"`
+	StudentID         int       `json:"studentID"`
+	Text              string    `json:"text,omitempty"`
+	CreateTime        time.Time `json:"createTime"`
+	File              string    `json:"file"`
+	Status            string    `json:"status"` // new | approve | reject
+	TeacherEvaluation string    `json:"teacherEvaluation"`
 }
 
 type SolutionListFromClass struct {
@@ -16,11 +18,13 @@ type SolutionListFromClass struct {
 }
 
 type SolutionForHw struct {
-	ID         int       `json:"id"`
-	StudentID  int       `json:"studentID"`
-	Text       string    `json:"text,omitempty"`
-	CreateTime time.Time `json:"createTime"`
-	File       string    `json:"file"`
+	ID                int       `json:"id"`
+	StudentID         int       `json:"studentID"`
+	Text              string    `json:"text,omitempty"`
+	CreateTime        time.Time `json:"createTime"`
+	File              string    `json:"file"`
+	Status            string    `json:"status"` // new | approve | reject
+	TeacherEvaluation string    `json:"teacherEvaluation"`
 }
 
 type SolutionListForHw struct {
@@ -28,11 +32,13 @@ type SolutionListForHw struct {
 }
 
 type SolutionByID struct {
-	HomeworkID int       `json:"hwID"`
-	StudentID  int       `json:"studentID"`
-	Text       string    `json:"text,omitempty"`
-	CreateTime time.Time `json:"createTime"`
-	File       string    `json:"file"`
+	HomeworkID        int       `json:"hwID"`
+	StudentID         int       `json:"studentID"`
+	Text              string    `json:"text,omitempty"`
+	CreateTime        time.Time `json:"createTime"`
+	File              string    `json:"file"`
+	Status            string    `json:"status"` // new | approve | reject
+	TeacherEvaluation string    `json:"teacherEvaluation"`
 }
 
 type SolutionByIDResponse struct {
--- repository/solution.go
+++ repository/solution.go
@@ -1,28 +1,43 @@
 package repository
 
 import (
+	"database/sql"
 	e "main/domain/errors"
 	"main/domain/model"
 )
 
+func makeStatus(statusFromDB sql.NullBool) string {
+	if statusFromDB.Valid {
+		if statusFromDB.Bool {
+			return "approve"
+		} else {
+			return "reject"
+		}
+	}
+	return "new"
+}
+
 func (s *Store) GetSolutionByID(id int) (*model.SolutionByID, error) {
 	var sol model.SolutionByID
+	var isApproved sql.NullBool
+
 	if err := s.db.QueryRow(
-		`SELECT homeworkID, studentID, text, createTime, file FROM solutions WHERE id = $1;`,
+		`SELECT homeworkID, studentID, text, createTime, file, isApproved, teacherEvaluation FROM solutions WHERE id = $1;`,
 		id,
 	).Scan(
 		&sol.HomeworkID, &sol.StudentID,
-		&sol.Text, &sol.CreateTime, &sol.File,
+		&sol.Text, &sol.CreateTime, &sol.File, &isApproved, &sol.TeacherEvaluation,
 	); err != nil {
 		return nil, e.StacktraceError(err)
 	}
+	sol.Status = makeStatus(isApproved)
 
 	return &sol, nil
 }
 
 func (s *Store) GetSolutionsByClassID(classID int) (*model.SolutionListFromClass, error) {
 	rows, err := s.db.Query(
-		`SELECT s.id, s.homeworkID, s.studentID, s.text, s.createTime, s.file
+		`SELECT s.id, s.homeworkID, s.studentID, s.text, s.createTime, s.file, s.isApproved, s.teacherEvaluation
 		 FROM solutions s
 		 JOIN homeworks h ON s.homeworkID = h.id
 		 WHERE h.classID = $1;`,
@@ -36,13 +51,16 @@ func (s *Store) GetSolutionsByClassID(classID int) (*model.SolutionListFromClass
 	sols := []*model.SolutionFromClass{}
 	for rows.Next() {
 		var tmpSol model.SolutionFromClass
+		var isApproved sql.NullBool
 
 		if err := rows.Scan(
 			&tmpSol.ID, &tmpSol.HomeworkID, &tmpSol.StudentID,
 			&tmpSol.Text, &tmpSol.CreateTime, &tmpSol.File,
+			&isApproved, &tmpSol.TeacherEvaluation,
 		); err != nil {
 			return nil, e.StacktraceError(err)
 		}
+		tmpSol.Status = makeStatus(isApproved)
 
 		sols = append(sols, &tmpSol)
 	}
@@ -52,7 +70,7 @@ func (s *Store) GetSolutionsByClassID(classID int) (*model.SolutionListFromClass
 
 func (s *Store) GetSolutionsByHomeworkID(homeworkID int) (*model.SolutionListForHw, error) {
 	rows, err := s.db.Query(
-		`SELECT id, studentID, text, createTime, file FROM solutions WHERE homeworkID = $1;`,
+		`SELECT id, studentID, text, createTime, file, isApproved, teacherEvaluation FROM solutions WHERE homeworkID = $1;`,
 		homeworkID,
 	)
 	if err != nil {
@@ -63,13 +81,16 @@ func (s *Store) GetSolutionsByHomeworkID(homeworkID int) (*model.SolutionListFor
 	sols := []*model.SolutionForHw{}
 	for rows.Next() {
 		var tmpSol model.SolutionForHw
+		var isApproved sql.NullBool
 
 		if err := rows.Scan(
 			&tmpSol.ID, &tmpSol.StudentID,
 			&tmpSol.Text, &tmpSol.CreateTime, &tmpSol.File,
+			&isApproved, &tmpSol.TeacherEvaluation,
 		); err != nil {
 			return nil, e.StacktraceError(err)
 		}
+		tmpSol.Status = makeStatus(isApproved)
 
 		sols = append(sols, &tmpSol)
 	}
