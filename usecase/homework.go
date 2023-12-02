ba8d6820be5012b0e617a5270d18d8402b6e848d
Leonard Pak <leopak2000@gmail.com>
2023-12-02 22:17:56 +0300

    fix broadcast msg

--- usecase/homework.go
+++ usecase/homework.go
@@ -72,6 +72,9 @@ func (uc Usecase) genHomeworkMsg(hw *model.HomeworkCreate) (*model.ClassBroadcas
 		}
 
 		msg.Description += "\n" + "Задание №" + strconv.Itoa(id+1) + "\n" + task.Description
+		if len(task.Attach) == 0 {
+			continue
+		}
 		msg.Attaches = append(msg.Attaches, task.Attach)
 	}
 
